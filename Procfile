d44c83a192251e8aa95e7e40660b036b5f824088
M-Tinati <tinatigithub@gmail.com>
2025-09-13 22:19:38 +0330

    Fix

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn poly.wsgi
--- poly/settings.py
+++ poly/settings.py
@@ -127,10 +127,15 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
